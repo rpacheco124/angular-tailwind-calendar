ade64aed3036e6af237a620851e91c0601bde97d
rpacheco124 <rpacheco124@gmail.com>
2024-01-16 10:13:25 +0100

    feat(ng-calendar): add tailwind

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { NxWelcomeComponent } from './nx-welcome.component';
+import { NgCalendarComponent } from '@angular-tailwind-calendar/ng-calendar';
 
 @Component({
   standalone: true,
-  imports: [NxWelcomeComponent, RouterModule],
+  imports: [NxWelcomeComponent, RouterModule, NgCalendarComponent],
   selector: 'angular-tailwind-calendar-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
